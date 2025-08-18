c27e84e0b5ef8d0dd46b261103bc3a59213664e0
Mark Wilson <mwilson1962@gmail.com>
2025-08-18 11:34:17 -0400

    Update Register32Bits.h

--- Register32Bits.h
+++ Register32Bits.h
@@ -1,3 +1,10 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2025 Mark Wilson
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
 #pragma once
 
 #include <bitset>
